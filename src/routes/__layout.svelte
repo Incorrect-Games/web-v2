ef7f081c9e97bbae8af3847999c19e4471a9d2b4
Dyrhoi <realarcoz@gmail.com>
2022-01-14 23:38:29 +0100

    Initial commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import "../app.css";
+</script>
+
+<nav>
+    <div class="container flex">
+        <span />
+        <ul class="flex ml-auto">
+            <li>
+                <a href="/blog">Blog</a>
+            </li>
+            <li>
+                <a href="/team">Team</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<main>
+    <slot />
+</main>
+
+<footer>
+    <p>
+        visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit
+    </p>
+</footer>
---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+	import Counter from '$lib/Counter.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<p class="text-red-400">Yo</p>
