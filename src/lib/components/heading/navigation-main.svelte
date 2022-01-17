c0b277358df369f35961cdb65c94990a7ed21f7c
Dyrhoi <realarcoz@gmail.com>
2022-01-17 21:32:26 +0100

    Add z-index to nav

--- src/lib/components/heading/navigation-main.svelte
+++ src/lib/components/heading/navigation-main.svelte
@@ -2,7 +2,7 @@
     import NavLink from "./nav-link.svelte";
 </script>
 
-<nav class="bg-white/80 backdrop-blur-lg sticky top-0 mt-6">
+<nav class="bg-white/80 backdrop-blur-lg sticky top-0 mt-6 z-30">
     <div class="container flex h-[64px] place-items-center">
         <a href="/">
             <span class="font-title text-black font-bold text-2xl">Incorrect</span>
