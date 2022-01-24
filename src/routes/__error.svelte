2da6cab6ea0b52c627cddfee30a70737772e0880
Dyrhoi <realarcoz@gmail.com>
2022-01-24 17:33:31 +0100

    Less padding on mobile. Fixed design structure of error pages.

---
+++ src/routes/__error.svelte
@@ -0,0 +1,26 @@
+<script context="module">
+    /** @type {import('@sveltejs/kit').ErrorLoad} */
+    export function load({ error, status }) {
+        return {
+            props: {
+                error,
+                status,
+            },
+        };
+    }
+</script>
+
+<script lang="ts">
+    export let error;
+    export let status;
+</script>
+
+<section>
+    <div class="container">
+        <h1>{status}</h1>
+        <p>{error.message}</p>
+        {#if error.stack}
+            <pre>{error.stack}</pre>
+        {/if}
+    </div>
+</section>
