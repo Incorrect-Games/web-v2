84650d087c4ac886c8e46458ea3fff7b0dde4d1c
Dyrhoi <realarcoz@gmail.com>
2022-01-26 23:35:58 +0100

    larger nav height

--- src/lib/components/heading/navigation-main.svelte
+++ src/lib/components/heading/navigation-main.svelte
@@ -2,9 +2,9 @@
     import NavLink from "./nav-link.svelte";
 </script>
 
-<nav class="bg-white/80 backdrop-blur-lg sticky top-0 mt-6 z-30 dark:bg-[#060606]/90">
-    <div class="container flex h-[64px] place-items-center">
-        <a href="/">
+<nav class="bg-white/80 backdrop-blur-lg sticky top-0 mt-4 z-30 dark:bg-[#060606]/90">
+    <div class="container flex h-[72px] place-items-center">
+        <a href="/" class="flex place-items-center gap-3">
             <span class="font-title text-black font-bold text-2xl dark:text-white">Incorrect</span>
         </a>
         <ul class="flex ml-auto gap-3">
